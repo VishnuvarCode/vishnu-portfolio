182c2eb2ec96348adcac391ab2b890bc8ffd8e79
VishnuvarCode <vishnurajvishnu517@gmail.com>
2024-07-16 13:23:00 +0530

    first commit

---
+++ src/data/user.js
@@ -0,0 +1,86 @@
+const INFO = {
+	main: {
+		title: "Reactportfolio by Vishnuraj",
+		name: "Vishnuraj V.",
+		email: "vishnuvishnu31221@gmail.com",
+		logo: "../logo.jpg",
+	}, 
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/VishnuvarCode",
+		linkedin: "https://www.linkedin.com/in/v-vishnuraj-2002v/",
+		instagram: "https://www.instagram.com/v_code_yeet/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Full-stack web and MERN-stack developer.",
+		description:
+			"Experienced React Developer with 2 years of expertise in modern web applications. Proficient in HTML, CSS, JavaScript, Bootstrap, jQuery, React.js, Redux, and React Router. Skilled in RESTful API integration and state management with Redux and Context API. Knowledgeable in npm, Git, responsive design, cross-browser compatibility, web performance optimization, and Progressive Web Apps. Strong team collaborator committed to delivering high-quality software solutions and staying current with development trends.",
+	},
+
+	about: {
+		title: "Hello, I'm Vishnuraj V. from Chennai, crafting tomorrow's innovations.",
+		description:
+			"Experienced React Developer with 2 years of expertise in modern web applications. Proficient in HTML, CSS, JavaScript, Bootstrap, jQuery, React.js, Redux, and React Router. Skilled in RESTful API integration and state management with Redux and Context API. Knowledgeable in npm, Git, responsive design, cross-browser compatibility, web performance optimization, and Progressive Web Apps. Strong team collaborator committed to delivering high-quality software solutions and staying current with development trends.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Employee attendance and Payroll application, Project 1",
+			description:
+				"Developed a financial application, Moneta, catering to personalized budgeting and expense tracking needs.",
+			logo: "../react-logo.png",
+			// logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			// linkText: "View Project",
+			// link: "https://github.com",
+		},
+
+		{
+			title: "Educational institution application, Project 2",
+			description:
+				"Implemented a web-based educational institutions application tailored for collaborative learning in educational institutions. The application facilitated communication and information dissemination among staff, heads of departments, and principals.",
+			logo: "../react-logo.png",
+			// linkText: "View Project",
+			// link: "https://github.com",
+		},
+
+		{
+			title: "Alloys, Project 3",
+			description:
+				"Collaborated on the development of an e-commerce platform for sports equipment.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/csharp/csharp.png",
+			// linkText: "View Project",
+			// link: "https://github.com",
+		},
+
+		{
+			title: "Sport cycle, Project 4",
+			description:
+				"This ASP.NET project allows clients to manage and track machine operations, purchase and payment details, spare parts inventory, and expenses. It generates comprehensive reports and provides a grid view for easy data visualization. Clients can also book slots via phone and email and keep track of customer details and payments.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/csharp/csharp.png",
+			// linkText: "View Project",
+			// link: "https://github.com",
+		},
+
+		{
+			title: "A TO Z motors, Project 5",
+			description:
+				"Contributed to the creation of a web-based inventory for a manufacturing company.",
+			logo: "../react-logo.png",
+			// logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			// linkText: "View Project",
+			// link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
